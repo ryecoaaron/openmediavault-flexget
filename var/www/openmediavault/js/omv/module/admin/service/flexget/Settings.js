33ee29d81bfe7d43c8ba133e777b891ca762df68
Aaron Murray <plugins@omv-extras.org>
2015-01-26 10:14:15 -0600

    add check syntax button and function

--- var/www/openmediavault/js/omv/module/admin/service/flexget/Settings.js
+++ var/www/openmediavault/js/omv/module/admin/service/flexget/Settings.js
@@ -6,6 +6,22 @@ Ext.define("OMV.module.admin.service.flexget.Settings", {
     rpcService: "Flexget",
     rpcGetMethod: "getSettings",
     rpcSetMethod: "setSettings",
+
+    getButtonItems : function() {
+        var me = this;
+        var items = me.callParent(arguments);
+        items.push({
+            id      : me.getId() + "-check",
+            xtype   : "button",
+            text    : _("Check Syntax"),
+            icon    : "images/check.png",
+            iconCls : Ext.baseCSSPrefix + "btn-icon-16x16",
+            scope   : me,
+            handler : Ext.Function.bind(me.onCheckButton, me, [ me ])
+        });
+        return items;
+    },
+
     // getFormItems is a method which is automatically called in the 
     // instantiation of the panel. This method returns all fields for 
     // the panel.
@@ -36,7 +52,38 @@ Ext.define("OMV.module.admin.service.flexget.Settings", {
                 allowBlank: true
             }]
         }];
+    },
+
+    onCheckButton: function() {
+        var me = this;
+        var config = me.findField("config").getValue();
+        var wnd = Ext.create("OMV.window.Execute", {
+            title           : _("Checking config ..."),
+            rpcService      : "Flexget",
+            rpcMethod       : "doCheckSyntax",
+            rpcParams      : {
+                "config" : config
+            },
+            rpcIgnoreErrors : true,
+            hideStartButton : true,
+            hideStopButton  : true,
+            listeners       : {
+                scope     : me,
+                finish    : function(wnd, response) {
+                    wnd.appendValue(_("Done..."));
+                    wnd.setButtonDisabled("close", false);
+                },
+                exception : function(wnd, error) {
+                    OMV.MessageBox.error(null, error);
+                    wnd.setButtonDisabled("close", false);
+                }
+            }
+        });
+        wnd.setButtonDisabled("close", true);
+        wnd.show();
+        wnd.start();
     }
+
 });
 // Register a panel into the GUI.
 //
